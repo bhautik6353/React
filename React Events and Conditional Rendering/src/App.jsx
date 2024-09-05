52e4ac9d940624a982fb3bc836f4c6859462c814
Bhautik Kapuriya <162436634+bhautik6353@users.noreply.github.com>
2024-09-05 15:56:34 +0530

    React Events and Conditional Rendering

---
+++ React Events and Conditional Rendering/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Events from './Component/Events'
+import EventConditions from './Component/EventConditions'
+
+export default function App() {
+  return (
+    <div>
+      <Events/>
+      <EventConditions/>
+    </div>
+  )
+}
