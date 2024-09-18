56ac4c0c45d4bc8f6882842369822a6db8732bec
Bhautik Kapuriya <162436634+bhautik6353@users.noreply.github.com>
2024-09-18 13:12:38 +0530

    Project Page Form Project

---
+++ Product page form project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Exmp from './Component/Exmp'
+import Productform from './Component/Productform'
+
+export default function App() {
+  return (
+    <div>
+      <Productform/>
+    </div>
+  )
+}
