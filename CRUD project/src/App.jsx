2a2e2874deb6a3f16d7bd07e7a4968bae4a34287
Bhautik Kapuriya <162436634+bhautik6353@users.noreply.github.com>
2024-09-30 10:26:57 +0530

    CRUD_Local-Storage_Project

---
+++ CRUD project/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Crudpro from './CrudProject/Crudpro'
+
+export default function App() {
+  return (
+    <div>
+      <Crudpro/>
+    </div>
+  )
+}
---
+++ CRUD project/src/CrudProject/Crudpro.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Crudpro() {
+    const[task,setTask]=useState("")
+
+    const[taskList,setTaskList]=useState([])
+    const[editTask,setEditTask]=useState(null)
+    
+
+    useEffect(()=>{
+      let alldata=JSON.parse(localStorage.getItem("task"))
+      setTaskList(alldata)
+    },[])
+
+    const addTask=()=>{
+      
+        let user={id:Date.now(),task,status:"Peddind"}
+        let olddata=JSON.parse(localStorage.getItem("task")) || []
+        if(editTask){
+          let editdata=taskList.find((item)=>item.id==editTask)
+          editdata.id=editTask
+          editdata.task=task
+          localStorage.setItem("task",JSON.stringify(taskList))
+          setEditTask(null)
+        }else{
+          olddata.push(user)
+          setTaskList(olddata)
+          localStorage.setItem("task",JSON.stringify(olddata))
+
+        }
+
+        setTask("")
+
+        
+
+    }
+    const deletetask=(id)=>{
+      let delet=taskList.filter((item)=>item.id !=id)
+      setTaskList(delet)
+      localStorage.setItem("task",JSON.stringify(delet))
+
+    }
+    const edittask=(id)=>{
+      let edit=taskList.find((item)=>item.id==id)
+      setTask(edit.task)
+      setEditTask(id)
+    }
+const editstatus=(id)=>{
+  const updatestatus= taskList.map((task)=> task.id == id ? {...task,status:"Complete"}:task)
+  setTaskList(updatestatus)
+  localStorage.setItem("task",JSON.stringify(updatestatus))
+}
+
+
+  return (
+    <div>
+      <center>
+    <div id='s4'>  <br />  <h1>Task Manager</h1>
+        <input type="text" value={task} onChange={(e)=>setTask(e.target.value)} placeholder="Enter your task"/>
+        <button onClick={addTask}>{editTask? "Update Task" :"Add Task"}</button> <br /><br />
+        <table>
+          <thead>
+            <th>Id</th>
+            <th>Task</th>
+            <th>Status</th>
+            <th colSpan={3}>Actions</th>
+          </thead>
+          <tbody>
+            {
+              taskList?
+              taskList.map((item,index)=>{
+                return <tr key={index}>
+                  <td>{item.id}</td>
+                  <td>{item.task}</td>
+                  <td>{item.status}</td>
+                  <td>
+                    <button id='s1' onClick={()=>editstatus(item.id)}>Complete</button>
+                    <button id='s3' onClick={()=>edittask(item.id)}>Edit</button>
+                    <button id='s2' onClick={()=>deletetask(item.id)}>Delete</button>
+                    
+
+
+                  </td>
+                </tr>
+              }):""
+            }
+          </tbody>
+        </table>
+        <br />
+        </div>
+      </center>
+      <br /><br /><br />
+    </div>
+  )
+}
