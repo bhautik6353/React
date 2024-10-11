6c28f2417b7a9c9c74586e0d38cc6b570fe3eb76
Bhautik Kapuriya <162436634+bhautik6353@users.noreply.github.com>
2024-10-11 15:50:46 +0530

    Readux CRUD

---
+++ Redux Crud/src/redux/Reduxcrud.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addStudent, deleteStudent } from '../slice/Slice'
+
+export default function Reduxcrud() {
+    const[name,setName]=useState("")
+    const[subject,setSubject]=useState("")
+
+    const data=useSelector((state)=>{
+        return state.studentkey
+    })
+
+    const dispatch=useDispatch();
+
+    const addRecord=()=>{
+        dispatch(addStudent({id:Date.now(),name,subject}))
+        setName("")
+        setSubject("")
+    }
+
+    const deleteRecord=(id)=>{
+    dispatch(deleteStudent(id))
+    }
+
+  return (
+    <div>
+      <center>
+        <h1>Redux Crud</h1>
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"/> <br /><br />
+        <input type="text" value={subject} onChange={(e)=>setSubject(e.target.value)} placeholder="Enter Subject"/> <br /><br />
+        <button onClick={addRecord}>Add Data</button> <br /><br />
+        <h2>Data</h2>
+        {
+            data ?
+            data.student.map((e,i)=>{
+                return <ul key={i}>
+                    <li>Id: {e.id}</li>
+                    <li>Name: {e.name}</li>
+                    <li>Subject: {e.subject}</li>
+                    <button onClick={()=>deleteRecord(e.id)}>Delete</button>
+
+
+                </ul>
+
+
+            }):
+            <p>No Data..........</p>
+        }
+        
+
+      </center>
+    </div>
+  )
+}
---
+++ Redux Crud/src/store/Store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import student  from "../slice/Slice";
+
+export const store=configureStore({
+    reducer:{
+        studentkey:student
+    }
+    
+})
